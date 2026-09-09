70e5dc7966d0964ad10ecdc4b1c7c774aacfafe3
Sam-P96 <samnap7@gmail.com>
2026-09-09 15:07:29 +0300

    feat(components): complete Tour removal functionalities

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,4 +1,4 @@
-function Tour({ image, date, title, info, location, duration, cost, difficulty, rating }) {
+function Tour({ id, image, date, title, info, location, duration, cost, difficulty, rating }) {
   return (
     <article className="tour-card">
       <div className="tour-img-container">
@@ -22,6 +22,13 @@ function Tour({ image, date, title, info, location, duration, cost, difficulty, 
           <p>from {cost}</p>
           <p>{duration} days</p>
         </div>
+        <button 
+        className='btn' 
+        onClick={() => onRemove(id)}
+        style={{ marginTop: '1rem' }}
+      >
+        Remove Tour
+      </button>
       </div>
     </article>
   );
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -5,13 +5,23 @@ import Tour from "./Tour";
 
 const Tours = () => {
   const [toursData, setToursData] = useState(tours);
+
+  const handleRemoveTour = (tourId) => {
+    setToursData(toursData.filter(tour => tour.id !== tourId));
+  };
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
         {toursData.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return (
+            <Tour
+              {...tour}
+              key={tour.id}
+              onRemove={handleRemoveTour}
+            />
+          )
         })}
       </div>
     </section>
